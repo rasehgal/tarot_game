a630976e14ebea49914eaa421a7ad23d9906ad3a
rasehgal <rsehgal95@gmail.com>
2023-07-31 15:50:48 -0700

    modify tarot.py for future usage

--- tarot.py
+++ tarot.py
@@ -10,14 +10,25 @@ TDECK = deck() #creates deck
 for name, desc in STARTER_PACK:
 	TDECK.add_card(name,desc)
 
+# define various tarot spreads in an interactive way, allow
+# for upside down cards, different orientations, and diff categories
+# and interpretations
+def three_spread(): return 0
+def five_spread(): return 0
+def future(): return 0
+def single_draw(): return 0
 
-#deck.three_spread()
-#deck.print_deck()
-#deck.three_spread()
-TDECK.print_deck()
-TDECK.add_card("foo", "bar")
-print("added")
-TDECK.print_deck()
-TDECK.remove_card("foo")
-print("removed")
-TDECK.print_deck()
+
+#eventually create interactivity 
+#why won't you add to my commit
+def main(): 
+    #deck.three_spread()
+    #deck.print_deck()
+    #deck.three_spread()
+    TDECK.print_deck()
+    TDECK.add_card("foo", "bar")
+    print("added")
+    TDECK.print_deck()
+    TDECK.remove_card("foo")
+    print("removed")
+    TDECK.print_deck()
