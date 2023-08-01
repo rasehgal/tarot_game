281bb86e1b4431ae1a16736bf902a322d117be9a
rasehgal <rsehgal95@gmail.com>
2023-08-01 15:23:03 -0700

    interactivity and beginnings of spreads

--- deck.py
+++ deck.py
@@ -1,8 +1,9 @@
 #basic deck class for tarot/etc
-
+import random
 
 #create class deck
 #deck: list of tuples containing card name and a definition/value
+#functions: print_deck, find_card, add_card, draw_cards, remove_card, shuffle_deck
 class deck:
     def __init__(self):
         #initialize empty list
@@ -30,14 +31,24 @@ class deck:
 
     # "draw" cards from the deck-- removes {count} list items off of the list and prints values
     #TO-DO take in whether to pop or replace
-    def draw_cards(self, count):
+    def draw_cards(self, count, replacement):
+        drawn = []
 		#if there are fewer than this many cards left, error
         if len(self.deck) < count: return 0
 		#iterate through the deck for the number of cards chosen (count)
         # print the cards for viewing and remove from the stack
         for card, defn in (self.deck[:count]):
-            print(card, '-->', defn)
-            self.deck.remove((card,defn))
+            #print(card, '-->', defn)
+            this_card = (card,defn)
+            if replacement == 0:
+                self.deck.remove(this_card)
+            #print("now: ")
+            #print(card, '-->, '+ defn)
+            drawn.append(this_card)
+        #print("finally")
+        #for card,defn in (self.deck):
+           #print(card + '-->' + 'defn')
+        return self.deck, drawn
             
     # remove card by way of indexing function
     # TO-DO; make this consistent across functions
@@ -47,7 +58,8 @@ class deck:
         return 0
 
     # shuffle deck, mixing order of cards for new drawing if desired
-    def shuffle(self): return 0
+    def shuffle_deck(self):
+        random.shuffle(self.deck)
 
     # TO-DO: have a user option to either pop cards off the list when drawing, or to "return" them to the deck
 
--- tarot.py
+++ tarot.py
@@ -2,7 +2,8 @@ from deck import deck
 
 
 #initialize deck with starter cards
-STARTER_PACK = [("the lovers","descrthelovers"), ("the healer","descrthehealer"), ("the tower","towerdesc"), ("card_four","means something"), ("card_five","something else")]
+#STARTER_PACK = [("the lovers","descrthelovers"), ("the healer","descrthehealer"), ("the tower","towerdesc"), ("card_four","means something"), ("card_five","something else")]
+STARTER_PACK = [("one","one"),("two","two"),("three","three"),("four","four"),("five","five"),("six","six"),("seven","seven"),("eight","eight"),("nine","nine"),("ten","ten")]
 
 TDECK = deck() #creates deck
 
@@ -13,22 +14,30 @@ for name, desc in STARTER_PACK:
 # define various tarot spreads in an interactive way, allow
 # for upside down cards, different orientations, and diff categories
 # and interpretations
-def three_spread(): return 0
-def five_spread(): return 0
-def future(): return 0
-def single_draw(): return 0
+def spread(n, r = None): 
+    if r == 0:
+        repl = r
+    else: repl = 1
+    decks = (TDECK.draw_cards(n, repl))
+    new_spread = decks[1]
+    for card, defn in new_spread:
+        print(card + "-->" + defn)
 
 
 #eventually create interactivity 
 #why won't you add to my commit
-def main(): 
-    #deck.three_spread()
-    #deck.print_deck()
-    #deck.three_spread()
-    TDECK.print_deck()
-    TDECK.add_card("foo", "bar")
-    print("added")
-    TDECK.print_deck()
-    TDECK.remove_card("foo")
-    print("removed")
-    TDECK.print_deck()
+#def main(): 
+#TDECK.print_deck()
+#print("DRAWING")
+#spread(5, 0)
+#print("DECK:")
+#TDECK.print_deck()
+#print("SHUFFLING")
+#TDECK.shuffle_deck()
+#TDECK.print_deck()
+#print("DRAWING")
+#spread(3,1)
+#print("DRAWING")
+#spread(2)
+#print("END WITH")
+#TDECK.print_deck()
